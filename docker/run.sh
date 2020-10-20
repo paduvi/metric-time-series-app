893d38dcd0a4dd6bac6c6eb2c376b0ad0b66e064
Viet Phan <vietphanduc@admicro.vn>
2020-10-20 12:00:31 +0700

    update script docker

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,4 @@
 docker run -d --rm --network host \
   -e POSTGRES_PASSWORD=postgres \
   --name timescaledb timescale/timescaledb:latest-pg12
-docker run --network host -d -p 3000:3000 --name grafanahost grafana/grafana:6.4.0
+docker run -d --rm --network host --name grafanahost grafana/grafana:6.4.0
