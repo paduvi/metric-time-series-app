42c053bfcaf45b0468263d975c7bd5ce3e9f2ac1
Viet Phan <vietphanduc@admicro.vn>
2020-10-20 12:10:10 +0700

    update grafana version

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,4 @@
 docker run -d --rm --network host \
   -e POSTGRES_PASSWORD=postgres \
   --name timescaledb timescale/timescaledb:latest-pg12
-docker run -d --rm --network host --name grafanahost grafana/grafana:6.4.0
+docker run -d --rm --network host --name grafana grafana/grafana:7.2.1
